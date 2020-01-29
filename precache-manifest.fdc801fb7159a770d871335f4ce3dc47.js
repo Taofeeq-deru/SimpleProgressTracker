05790dbdcc83b2cdd0840399fad5845906e0f324
Taofeeq-deru <oluderutaofeeq@gmail.com>
2020-01-30 00:09:09 +0100

    Updates

---
+++ precache-manifest.fdc801fb7159a770d871335f4ce3dc47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f6422237ca34a525181ad2720e2171",
+    "url": "/SimpleProgressTracker/index.html"
+  },
+  {
+    "revision": "fed3b0692db5d46a32a6",
+    "url": "/SimpleProgressTracker/static/js/2.39889302.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SimpleProgressTracker/static/js/2.39889302.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fbee336303ff791d9bf",
+    "url": "/SimpleProgressTracker/static/js/main.b26687ae.chunk.js"
+  },
+  {
+    "revision": "185d5bc537b77e047647",
+    "url": "/SimpleProgressTracker/static/js/runtime-main.adb2f4ed.js"
+  }
+]);
